d0a474ea4ba4e425496cc30fb701e0ba93108636
maneesha-28 <manishanairnair2002@gmail.com>
2022-06-08 14:52:10 +0530

    committed

---
+++ src/components/body/body.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { ArrowDown } from "react-feather";
+import styles from "./body.module.css";
+
+function Body() {
+    const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
+    return (
+        <div className={styles.container}>
+            <p className={styles.heading}>Resume Builder</p>
+            <div className={styles.toolbar}>
+                <div className={styles.colors}>
+                    {colors.map((item) => (
+                        <span
+                            key={item}
+                            style={{ backgroundColor: item }}
+                            className={styles.color}
+
+                        />
+                    ))}
+                </div>
+                <button>Download <ArrowDown/>
+
+                </button>
+            </div>
+        </div>
+    );
+}
+export default Body;
